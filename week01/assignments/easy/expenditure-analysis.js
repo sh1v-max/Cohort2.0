76d19d848f3a378e9e9f391bbd6aa478e795476c
sh1v-max <singhshiv0427@gmail.com>
2026-01-07 22:42:32 +0530

    cohort continued, javascript

--- week01/assignments/easy/expenditure-analysis.js
+++ week01/assignments/easy/expenditure-analysis.js
@@ -13,71 +13,75 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Calculate the total price spent by category from a list of transactions
+ * @param {Array<Transaction>} Transactions - an array of objects with id, timestamp, price, category and itemName
+ * @returns {Array<Object>} - an array of objects where each object is unique category-wise and has total price spent as its value
+ */
+/*******  b6f281d7-8ab1-497a-8f1f-0129a46b78ab  *******/
 function calculateTotalSpentByCategory(Transactions) {
-  let catTotal= [];
+  let catTotal = []
 
   Transactions.forEach((T) => {
-    if(catTotal.hasOwnProperty(T.category)){
-      catTotal[T.category] = catTotal[T.category] + T.price;
+    if (catTotal.hasOwnProperty(T.category)) {
+      catTotal[T.category] = catTotal[T.category] + T.price
+    } else {
+      catTotal[T.category] = T.price
     }
-    else{
-      catTotal[T.category] = T.price;
-    }
-  });
-  
-//even shorter way
+  })
+
+  //even shorter way
   // Transactions.forEach((T) => {
   //   catTotal.hasOwnProperty(T.category) ? (catTotal[T.category] += T.price) : (catTotal[T.category] = T.price);
   // });
 
-    return catTotal;
-  };
+  return catTotal
+}
 
 const transactions = [
   {
     itemName: 'Pizza',
     category: 'Food',
     price: 300,
-    timestamp: 1656
+    timestamp: 1656,
   },
   {
     itemName: 'Jeans',
     category: 'Clothing',
     price: 5000,
-    timestamp: 1657
+    timestamp: 1657,
   },
   {
     itemName: 'Linkedin Premium',
     category: 'Career',
     price: 450,
-    timestamp: 1658
+    timestamp: 1658,
   },
   {
     itemName: 'Netflix',
     category: 'Entertainment',
     price: 2000,
-    timestamp: 1659
+    timestamp: 1659,
   },
   {
     itemName: 'Panipuri',
     category: 'Food',
     price: 100,
-    timestamp: 1660
+    timestamp: 1660,
   },
   {
     itemName: 'Disney+',
     category: 'Entertainment',
     price: 1000,
-    timestamp: 1661
+    timestamp: 1661,
   },
   {
     itemName: 'Twitter Blue',
     category: 'Career',
     price: 650,
-    timestamp: 1662
-  }
+    timestamp: 1662,
+  },
 ]
 
-
-module.exports = calculateTotalSpentByCategory;
+module.exports = calculateTotalSpentByCategory
