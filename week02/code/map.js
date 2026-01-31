dda46b0e62b7169f52662ab312d088a9fbba04d4
sh1v-max <singhshiv0427@gmail.com>
2026-01-31 16:37:24 +0530

    map

--- week02/code/map.js
+++ week02/code/map.js
@@ -1,48 +1,40 @@
-//  map in javascript is a collection of key-value pairs. 
-//  The keys are unique and the values can hold any type of values.
-//  The keys are used to retrieve the values.
+// given an array, give me back a new array in which every value is multiplied by 2
 
+const arr = [1, 2, 3, 4, 5]
 
-// Given an array, give me back a new array in which every value is multiplied by 2.
-
-const arr = [1, 2, 3, 4, 5];
-
-
-//1.  Using for loop
-const newArrLoop = [];
-for(let i = 0; i < arr.length; i++) {
-    newArrLoop.push(arr[i] * 2);
+// using for loop
+const newArrLoop = []
+for (let i = 0; i < arr.length; i++) {
+  newArrLoop.push(arr[i] * 2)
 }
-console.log("Using Loop " + newArrLoop)
+console.log('Using Loop ' + newArrLoop)
 
-// 2. 
-function transform(i){
-    return i * 2;
+// this is exactly what map let us do
+// using an intermediate function
+function transform(i) {
+  return i * 2
 }
-const ans = arr.map(transform);
-console.log(ans);
+const ans = arr.map(transform)
+console.log(ans)
 
-
-//3.  Using map
+// using map
 const newArr = arr.map((value) => {
-    return value * 2;
-});
+  return value * 2
+})
 
-console.log("Using Map " + newArr);
+console.log('Using Map ' + newArr)
 
-
-// 4.
-// List the fullname of all the users
+// list the fullname of all the users
 const users = [
-    {firstname:"Chandan", lastname:"Kushwaha", age:22},
-    {firstname:"Anjali", lastname:"Kumar", age:22},
-    {firstname:"RajKumar", lastname:"Kush", age:33},
-    {firstname:"Ram", lastname:"Sita", age:43},
-    {firstname:"Suraj", lastname:"waha", age:50},
-    {firstname:"Anshu", lastname:"Verma", age:25},
-];
+  { firstName: 'John', lastName: 'Smith', age: 22 },
+  { firstName: 'Emily', lastName: 'Johnson', age: 22 },
+  { firstName: 'William', lastName: 'Williams', age: 33 },
+  { firstName: 'Oliver', lastName: 'Brown', age: 43 },
+  { firstName: 'Ethan', lastName: 'Davis', age: 50 },
+  { firstName: 'Lily', lastName: 'Miller', age: 25 },
+]
 
 const fullname = users.map((user) => {
-    return user.firstname + " " + user.lastname;
-});
-console.log(fullname);
+  return user.firstName + ' ' + user.lastName
+})
+console.log(fullname)
