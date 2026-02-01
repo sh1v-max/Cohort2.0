4e1c24253992b6f15a17f53bca0ae76f7d248b1a
sh1v-max <singhshiv0427@gmail.com>
2026-02-01 15:45:22 +0530

    added listen page at /, just for clarification

--- week02/assignments/02-nodejs/solutions/todoServer.solution.file.js
+++ week02/assignments/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,11 +1,28 @@
+// this creates a simple RESTful API for managing a todo list using Node.js and Express
+
+// 1. GET /todos - Returns a list of todos
+// 2. GET /todos/:id - Returns a single todo by ID
+// 3. POST /todos - Creates a new todo
+// 4. PUT /todos/:id - Updates a todo by ID
+// 5. DELETE /todos/:id - Deletes a todo by ID
+
+// all data is stored in a local JSON file named todos.json
+// it's called "file based persistence storage"
+
 const express = require('express')
+// node cannot read json body by default, we need body-parser middleware
+// it converts request body into usable json object
 const bodyParser = require('body-parser')
+// file system module to read and write files
 const fs = require('fs')
 
 const app = express()
-
+// use body-parser middleware to parse json body
+// it parse json and attach it to req.body
+// without this, req.body === undefined
 app.use(bodyParser.json())
 
+// helper function to find index of todo by id
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].id === id) return i
@@ -13,6 +30,7 @@ function findIndex(arr, id) {
   return -1
 }
 
+// helper function to remove item at index from array
 function removeAtIndex(arr, index) {
   let newArray = []
   for (let i = 0; i < arr.length; i++) {
@@ -21,6 +39,8 @@ function removeAtIndex(arr, index) {
   return newArray
 }
 
+// define routes
+// root route to show api status
 app.get('/', (req, res) => {
   res.json({
     status: 'ok',
@@ -35,6 +55,8 @@ app.get('/', (req, res) => {
   })
 })
 
+// CRUD routes for todos
+// shows all todos
 app.get('/todos', (req, res) => {
   fs.readFile('todos.json', 'utf8', function (err, data) {
     if (err) throw err
@@ -42,22 +64,29 @@ app.get('/todos', (req, res) => {
   })
 })
 
+// shows single todo by id
 app.get('/todos/:id', (req, res) => {
   fs.readFile('todos.json', 'utf8', function (err, data) {
     if (err) throw err
+    // data is not js array yet, it's string
     const todos = JSON.parse(data)
+    // extract id from req.params.id, it is string, convert to int
     const todoIndex = findIndex(todos, parseInt(req.params.id))
+    // findIndex returns 1 if found, -1 if not found
     if (todoIndex === -1) {
       res.status(404).send()
     } else {
+      // return the found todo
       res.json(todos[todoIndex])
     }
   })
 })
 
+// creates new todo
 app.post('/todos', function (req, res) {
   const newTodo = {
-    id: Math.floor(Math.random() * 1000000), // unique random id
+    // unique random id
+    id: Math.floor(Math.random() * 10000),
     title: req.body.title,
     description: req.body.description,
   }
