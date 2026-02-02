ac3b1a70a4b2710e26a3f50b36db3b6dc21211cf
sh1v-max <singhshiv0427@gmail.com>
2026-02-02 23:06:54 +0530

    middleware authentication

--- week03/mynotes/01-middleware-auth-globalcatches-zod/01-middleware.js
+++ week03/mynotes/01-middleware-auth-globalcatches-zod/01-middleware.js
@@ -1,14 +1,12 @@
-// Middleware is a function that runs between the request and the response. 
-// It can be used to check if the user is logged in or not, if the user has the right permissions to access the data, etc. 
-// It is a very powerful tool to use in your application. 
-
-// Using middleware to check if the user is logged in or not.
+// middleware is a function that runs between the request and the response
+// it can be used for authentication, logging, etc
+// here we will create a simple middleware for authentication
 
 const express = require('express');
 const app = express();
 
 app.get('/health-checkup', (req, res) => {
-    // Do health check here
+    // doing simple authentication
     const username = req.headers.username;
     const password = req.headers.password;
     const kidneyId = req.query.kidneyId;
@@ -36,8 +34,6 @@ app.get('/health-checkup', (req, res) => {
 
 app.listen(3000);
 
-/*
-Run - node index.js
-url - localhost:3000/health-checkup?kidneyId=1 (change kidneyId to 2 and 3 to see different outputs)
-headers - username: admin, password: admin (use postman)
-*/
+// run - node index.js
+// url - localhost:3000/health-checkup?kidneyId=1 (change kidneyId to 2 and 3 to see different outputs)
+// headers - username: admin, password: admin (use postman)
