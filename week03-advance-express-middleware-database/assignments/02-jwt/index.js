4211165ef85664b1ff4ca1242f4e61c7ae429c5c
sh1v-max <singhshiv0427@gmail.com>
2026-02-19 19:35:41 +0530

    jwt

--- week03-advance-express-middleware-database/assignments/02-jwt/index.js
+++ week03-advance-express-middleware-database/assignments/02-jwt/index.js
@@ -1,53 +1,55 @@
-const jwt = require('jsonwebtoken');
-const jwtPassword = 'secret';
+const jwt = require('jsonwebtoken')
+const jwtPassword = 'secret'
 
-const { z } = require('zod');
+const { z } = require('zod')
 
+// generating a jwt for a user with the given username and password
+
+// in this function, we will first validate the username and password using zod, and if they are valid, we will generate a jwt and return it... if they are not valid, we will return null
 function signJwt(username, password) {
-    const schema = z.object({
-        username: z.string().email(),
-        password: z.string().min(6),
-    })
-    const payload = {
-        username,
-        password
-    }
-    let result = schema.safeParse(payload)
-    if (!result.success) {
-        return null
-    }else{
-        return jwt.sign(payload, jwtPassword)
-    }
-
+  const schema = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+  })
+  const payload = {
+    username,
+    password,
+  }
+  let result = schema.safeParse(payload)
+  if (!result.success) {
+    return null
+  } else {
+    return jwt.sign(payload, jwtPassword)
+  }
 }
 
+// verifying a jwt token
+
 function verifyJwt(token) {
-    // Your code here
-    try {
-        jwt.verify(token, jwtPassword)
-        return true;
-    } catch (error) {
-        return false
-    }
+  // Your code here
+  try {
+    jwt.verify(token, jwtPassword)
+    return true
+  } catch (error) {
+    return false
+  }
 }
 
 function decodeJwt(token) {
-    // Your code here
-    try {
-        const decoded = jwt.decode(token)
-        if (!decoded) {
-            return false
-        }
-        return true;
-    } catch (error) {
-        return false
+  try {
+    const decoded = jwt.decode(token)
+    if (!decoded) {
+      return false
     }
+    return true
+  } catch (error) {
+    return false
+  }
 }
-
 
 module.exports = {
   signJwt,
   verifyJwt,
   decodeJwt,
   jwtPassword,
-};
+}
