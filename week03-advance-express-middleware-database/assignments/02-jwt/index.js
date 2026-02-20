5d19bd56a5ff9ce21b36b43fed631811d3713c50
sh1v-max <singhshiv0427@gmail.com>
2026-02-20 18:57:28 +0530

    practice jwt and zod

--- week03-advance-express-middleware-database/assignments/02-jwt/index.js
+++ week03-advance-express-middleware-database/assignments/02-jwt/index.js
@@ -7,6 +7,7 @@ const { z } = require('zod')
 
 // in this function, we will first validate the username and password using zod, and if they are valid, we will generate a jwt and return it... if they are not valid, we will return null
 function signJwt(username, password) {
+  // validating the username and password using zod
   const schema = z.object({
     username: z.string().email(),
     password: z.string().min(6),
@@ -19,14 +20,31 @@ function signJwt(username, password) {
   if (!result.success) {
     return null
   } else {
+    // else, we will generate a jwt and return it
     return jwt.sign(payload, jwtPassword)
   }
 }
 
-// verifying a jwt token
+// decoding a jwt token, here we will return true if the token is valid,
+// and false if it is not valid... note that this function should not throw an error,
+// it should return false if the token is not valid, and true if it is valid
+// whatever the token might be, it can be decoded back to the original payload
 
+function decodeJwt(token) {
+  try {
+    // notice that decoding doesn't need the secret
+    const decoded = jwt.decode(token)
+    if (!decoded) {
+      return false
+    }
+    return true
+  } catch (error) {
+    return false 
+  }
+}
+
+// verifying a jwt token, here we will return true if the token is valid, and false if it is not valid
 function verifyJwt(token) {
-  // Your code here
   try {
     jwt.verify(token, jwtPassword)
     return true
@@ -35,17 +53,6 @@ function verifyJwt(token) {
   }
 }
 
-function decodeJwt(token) {
-  try {
-    const decoded = jwt.decode(token)
-    if (!decoded) {
-      return false
-    }
-    return true
-  } catch (error) {
-    return false
-  }
-}
 
 module.exports = {
   signJwt,
