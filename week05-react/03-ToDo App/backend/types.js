5fe571c7b70718c0a6cd4f0418e2ba23375bc9dc
sh1v-max <singhshiv0427@gmail.com>
2026-04-01 15:53:37 +0530

    types done

---
+++ week05-react/03-ToDo App/backend/types.js
@@ -0,0 +1,26 @@
+const zod = require("zod");
+
+// body - POST
+// {
+//   title: "string",
+//   description: "string"
+// }
+// body - PUT
+// {
+//   id: "string"
+// }
+
+const createTodo = zod.object({
+  title: zod.string(),
+  description: zod.string()
+})
+
+const updateTodo = zod.object({
+  id: zod.string()
+})
+
+// exporting the types
+module.exports = {
+  createTodo: createTodo,
+  updateTodo: updateTodo
+}
